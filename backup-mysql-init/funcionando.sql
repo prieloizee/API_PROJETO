27c929853e2985197925a8139dc38248002de5b3
gabriel1324t <gabriel.ignacio.carrasco1325@gmail.com>
2025-08-15 11:29:06 -0300

    Atualização

--- backup-mysql-init/funcionando.sql
+++ backup-mysql-init/funcionando.sql
@@ -33,7 +33,7 @@ CREATE TABLE `avaliacoes` (
   PRIMARY KEY (`id_avaliacao`),
   KEY `id_usuario` (`id_usuario`),
   CONSTRAINT `avaliacoes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,4 +155,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-15 10:51:53
+-- Dump completed on 2025-08-15  9:45:25
