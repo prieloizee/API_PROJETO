470853a2a95e48f19f33176d314bdead2153a0ba
gabriel1324t <gabriel.ignacio.carrasco1325@gmail.com>
2025-08-26 18:21:09 -0300

    Update googleMapsService.js

--- src/services/googleMapsService.js
+++ src/services/googleMapsService.js
@@ -3,7 +3,9 @@ const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 exports.buscarEstabelecimentosGoogle = async (location, radius, type) => {
   try {
+    //Procura na api os estabelecimentos
     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&key=${GOOGLE_API_KEY}&language=pt-BR`;
+    //envia um get para a api do google
     const response = await axios.get(url);
     return response.data.results;
   } catch (error) {
