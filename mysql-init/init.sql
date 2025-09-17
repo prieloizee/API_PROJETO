724fd94fb536c45f48ff98983fc2574c1ada6d44
Arthur <arthurcintrafaleiros99@gmail.com>
2025-09-17 11:15:54 -0300

    modificação no banco

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,49 +1,21 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
--- CREATE DATABASE  IF NOT EXISTS `projeto_final` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
--- USE `projeto_final`;
+CREATE DATABASE  IF NOT EXISTS `projeto_final` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `projeto_final`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
 -- Host: localhost    Database: projeto_final
 -- ------------------------------------------------------
--- Server version	8.0.43
+-- Server version 8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `avaliacoes`
---
-
-DROP TABLE IF EXISTS `avaliacoes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `avaliacoes` (
-  `id_avaliacao` int NOT NULL AUTO_INCREMENT,
-  `id_usuario` int NOT NULL,
-  `google_place_id` varchar(255) NOT NULL,
-  `comentario` text NOT NULL,
-  `nota` TINYINT NOT NULL CHECK (nota BETWEEN 1 AND 5),
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_avaliacao`),
-  KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `avaliacoes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `avaliacoes`
---
-
-LOCK TABLES `avaliacoes` WRITE;
-/*!40000 ALTER TABLE `avaliacoes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `avaliacoes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `estabelecimentos`
@@ -63,17 +35,62 @@ CREATE TABLE `estabelecimentos` (
   `longitude` decimal(10,7) DEFAULT NULL,
   `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `atualizado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`place_id`)   
+  PRIMARY KEY (`place_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `estabelecimentos`
 --
+DROP TABLE IF EXISTS `avaliacoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+
+CREATE TABLE `avaliacoes` (
+  `id_avaliacao` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `google_place_id` varchar(255) NOT NULL,
+  `comentario` text NOT NULL,
+  `nota` TINYINT NOT NULL CHECK (nota BETWEEN 1 AND 5),
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_avaliacao`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `avaliacoes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 LOCK TABLES `estabelecimentos` WRITE;
 /*!40000 ALTER TABLE `estabelecimentos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estabelecimentos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favoritos`
+--
+
+DROP TABLE IF EXISTS `favoritos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `favoritos` (
+  `id_favorito` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `google_place_id` varchar(255) NOT NULL,
+  `nome_estabelecimento` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_favorito`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favoritos`
+--
+
+LOCK TABLES `favoritos` WRITE;
+/*!40000 ALTER TABLE `favoritos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favoritos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -118,6 +135,8 @@ CREATE TABLE `usuario` (
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `cpf` char(11) NOT NULL,
+  `imagem` longblob,
+  `tipo_imagem` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `cpf` (`cpf`)
@@ -128,20 +147,19 @@ CREATE TABLE `usuario` (
 -- Dumping data for table `usuario`
 --
 
- CREATE TABLE favoritos (
-   id_favorito INT AUTO_INCREMENT PRIMARY KEY,
-   id_usuario INT NOT NULL,
-   google_place_id VARCHAR(255) NOT NULL,
-   nome_estabelecimento VARCHAR(255),
-   endereco VARCHAR(255),
-   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
-  );
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Jo├úo Silva','joao.silva@example.com','senha123','16123456789'),(2,'Maria Oliveira','maria.oliveira@example.com','senha123','16987654321'),(3,'Carlos Pereira','carlos.pereira@example.com','senha123','16123987456'),(4,'Ana Souza','ana.souza@example.com','senha123','16456123789'),(5,'Pedro Costa','pedro.costa@example.com','senha123','16789123456'),(6,'Laura Lima','laura.lima@example.com','senha123','16321654987'),(8,'pri','pri24@pri','$2b$10$1SgaPMrX9KN/Bxc..aDHvu5fI3PzvqxbED2v0OMLVR6sBSdH6jge.','12345678909');
+INSERT INTO `usuario` VALUES (1,'Jo├úo Silva','joao.silva@example.com','senha123','16123456789',NULL,NULL),(2,'Maria Oliveira','maria.oliveira@example.com','senha123','16987654321',NULL,NULL),(3,'Carlos Pereira','carlos.pereira@example.com','senha123','16123987456',NULL,NULL),(4,'Ana Souza','ana.souza@example.com','senha123','16456123789',NULL,NULL),(5,'Pedro Costa','pedro.costa@example.com','senha123','16789123456',NULL,NULL),(6,'Laura Lima','laura.lima@example.com','senha123','16321654987',NULL,NULL),(8,'pri','pri24@pri','$2b$10$1SgaPMrX9KN/Bxc..aDHvu5fI3PzvqxbED2v0OMLVR6sBSdH6jge.','12345678909',NULL,NULL);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'projeto_final'
+--
+
+--
+-- Dumping routines for database 'projeto_final'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -152,4 +170,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-15 14:18:54
+-- Dump completed on 2025-08-27 16:16:41
